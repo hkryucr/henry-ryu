f8e476780a2ad287bd507c2147bd10d5194579a0
Henry Ryu <hkryucr@gmail.com>
2020-04-28 01:05:29 -0700

    change title and contents

--- src/main.jsx
+++ src/main.jsx
@@ -15,18 +15,18 @@ class Main extends React.Component {
 
     getInput (number){
       const input1 = {
-        title: "Chicken Tinder",
+        title: "Eat Together (Chicken Tinder)",
         gitLink: "https://github.com/hkryucr/mern-ct",
         liveLink: "https://lit-atoll-81167.herokuapp.com/",
         stacks: "(Node.js, Express.js, MongDB, React/Flux, JavaScript, CSS)",
         info:
-          "A web app inspired by Tinder to help groups make decisions on restaurants or bars.",
+          "A web app to help groups match everyone's dietary preferences and location to restaurants and bars.",
         infoFeatures: [
-          "Improved user experience by implementing a fully responsive, device-agnostic design using media query and flexbox.",
-          "Removed unnecessary scrolling for users by creating a custom modal framework using flux architecture.",
-          "Integrated Mapbox API with custom search functionality to dynamically present business locations based on the shown neighborhood on the map.",
-          "Implemented secure user authentication by utilizing local strategy with Passport.js and BCrypt for password hashing.",
-          "Architected highly scalable and uni-directional front-end state management by incorporating React-Redux container."
+          "Architected SPA (Single-Page Application) using best practices in state management by incorporating React with Redux.",
+          "Implemented fully responsive, device-agnostic front end web page using React, CSS flexbox, and media queries.",
+          "Integrated search feature with Mapbox API to show business locations in a map view.",
+          "Constructed user authentication with Passport.js and BCrypt for password hashing.",
+          "Conducted code reviews and assisted other team members with building UI."
         ],
         imgUrl: "https://portfolio-henry.s3-us-west-1.amazonaws.com/CT_Overview.gif",
       };
@@ -39,12 +39,11 @@ class Main extends React.Component {
         info:
           "A clone app showcasing the major features and functions on Yelp.",
         infoFeatures: [
-          "Merged real data from Yelp Fusion API with the project’s database to make the data seeding process more efficient.",
+          "Produced reusable react components to mimic Yelp's design system.",
           "Integrated geolocation-based searching with Google Maps API to display the location of businesses on a map.",
-          "Maximized user experience by front-loading search keywords within auto-complete search bars with native JavaScript.",
-          "Constructed numerous React components including forms, carousels, and reviews to imitate Yelp’s interface.",
-          "Created function where users can upload photos by utilizing AWS S3 and Active Storage for scalability.",
-          "Optimized database usage by eliminating N+1 queries within Active Record associations.",
+          "Integrated Yelp Fusion API with Ruby on Rails backend to display restaurants.",
+          "Implemented a fast search feature that auto-completes by front-loaded results and filtering by Regular Expressions.",
+          "Developed feature for users to upload their images to the AWS S3 backend that is optimized with Active Storage.",
           "Built full user authentication for signup/login using BCrypt",
         ],
         imgUrl: "https://portfolio-henry.s3-us-west-1.amazonaws.com/Yocal_Overall_App.gif",
@@ -56,8 +55,8 @@ class Main extends React.Component {
         stacks: "(JavaScript, CSS, HTML)",
         info: "A single page app that visualizes path finding algorithms.",
         infoFeatures: [
-          "Applied pure CSS and HTML to build a three-dimensional board and tiles for improved visualization.",
-          "Built a tree data structure to find the shortest path from the start node to the end node by implementing different types of algorithms.",
+          "Implemented efficient algorithms such as Dijkstra's algorithm to find the shortest path of nodes.",
+          "Created an easy-to-understand visualization to display the shortest path using javascript and CSS.",
         ],
         imgUrl: "https://portfolio-henry.s3-us-west-1.amazonaws.com/Path_Finder.gif",
       };
@@ -95,7 +94,7 @@ class Main extends React.Component {
                   onClick={this.handleClick(0)}
                 >
                   <div className="main-contents-tab-header">
-                    <span> Chicken Tinder</span>
+                    <span> Eat Together</span>
                     <FontAwesomeIcon
                       icon={faAngleDoubleRight}
                       color="#b0b0b0"
